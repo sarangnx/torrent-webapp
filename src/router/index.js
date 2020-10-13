e58eb5a8e7b106f0d60262f2a1982d47040c9bdf
Sarang <sarang98divakaran@gmail.com>
2020-10-13 13:34:27 +0530

    🌟 feat: added scroll behaviour, removed about route

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,25 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: () => import(/* webpackChunkName: "home" */ '@/views/Home')
+    }
 ];
 
+function scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+        return savedPosition;
+    } else {
+        return { top: 0, behavior: 'smooth' };
+    }
+}
+
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
+    scrollBehavior
 });
 
 export default router;
