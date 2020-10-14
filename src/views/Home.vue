6945b46988c92934167aa42e30a7f9ebf7a5cb59
Sarang <sarang98divakaran@gmail.com>
2020-10-14 20:55:35 +0530

    🌟 feat: added image and grid layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
     <top-bar />
-    <div class="home"></div>
+    <div class="home">
+        <div class="text-container"></div>
+        <div class="ship-container">
+            <img src="@/assets/images/pirate-ship.svg" />
+        </div>
+    </div>
 </template>
 
 <script>
@@ -12,3 +17,20 @@ export default {
     }
 };
 </script>
+
+<style lang="scss">
+.home {
+    display: grid;
+    grid-template-columns: 1fr 5fr 5fr 1fr;
+    grid-template-areas: '. left right .';
+    .text-container {
+        grid-area: left;
+    }
+    .ship-container {
+        grid-area: right;
+        img {
+            width: 100%;
+        }
+    }
+}
+</style>
