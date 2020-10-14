125fe42eac7065b1e0a570f2322fecd4dec7f0b1
Sarang <sarang98divakaran@gmail.com>
2020-10-14 20:24:57 +0530

    🌟 feat: added topbar component

---
+++ src/components/TopBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="top-bar">
+        <span class="title">BLACK PEARL</span>
+    </div>
+</template>
+
+<style lang="scss">
+.top-bar {
+    padding: 1.5rem 2rem;
+    .title {
+        font-weight: bold;
+        letter-spacing: 0.25rem;
+    }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,14 @@
 <template>
+    <top-bar />
     <div class="home"></div>
 </template>
+
+<script>
+import TopBar from '@/components/TopBar';
+
+export default {
+    components: {
+        TopBar
+    }
+};
+</script>
