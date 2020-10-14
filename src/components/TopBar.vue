8c54ca0657b23b4e4981f0a1f012bf4ddb61accf
Sarang <sarang98divakaran@gmail.com>
2020-10-14 22:20:48 +0530

    🌟 feat: align topbar contents using grids

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -6,8 +6,11 @@
 
 <style lang="scss">
 .top-bar {
-    padding: 1.5rem 2rem;
+    padding: 1.5rem 0rem;
+    display: grid;
+    grid-template-columns: 1fr 10fr 1fr;
     .title {
+        grid-column: 2;
         font-weight: bold;
         letter-spacing: 0.25rem;
     }
