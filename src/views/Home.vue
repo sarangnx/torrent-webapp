814536b046ad5939c96d54c5f8e6fd43ac456e64
Sarang <sarang98divakaran@gmail.com>
2020-10-14 22:15:28 +0530

    🌟 feat: grids with media queries

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,17 @@
 <template>
     <top-bar />
     <div class="home">
-        <div class="text-container"></div>
+        <div class="text-container">
+            <div class="title">
+                <span>Dowload Torrents</span>
+                <span>To Google Drive</span>
+            </div>
+            <div class="sub-title">
+                <span>Faster downloads</span>
+                <span>No installs required</span>
+                <span>No additional softwares required</span>
+            </div>
+        </div>
         <div class="ship-container">
             <img src="@/assets/images/pirate-ship.svg" />
         </div>
@@ -23,13 +33,49 @@ export default {
     display: grid;
     grid-template-columns: 1fr 5fr 5fr 1fr;
     grid-template-areas: '. left right .';
+
     .text-container {
         grid-area: left;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        .title {
+            display: grid;
+            font-size: 48px;
+            font-weight: bold;
+        }
+        .sub-title {
+            display: grid;
+            font-size: 1rem;
+            color: #676767;
+        }
     }
     .ship-container {
         grid-area: right;
         img {
             width: 100%;
+        }
+    }
+}
+
+// For mobile
+@media (max-width: 768px) {
+    .home {
+        grid-template-rows: 1fr 1fr;
+        .text-container {
+            grid-column: 2 / span 2;
+            grid-row: 1;
+            text-align: center;
+            .title {
+                font-size: 30px;
+            }
+            .sub-title {
+                font-size: 1rem;
+            }
+        }
+        .ship-container {
+            grid-column: 2 / span 2;
+            grid-row: 2;
         }
     }
 }
