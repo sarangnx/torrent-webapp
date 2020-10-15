d2133fa518bacf7b240bfb84c9cf886ac5bce563
Sarang <sarang98divakaran@gmail.com>
2020-10-15 15:42:53 +0530

    🌟 feat: empty page placeholder component

---
+++ src/components/EmptyPage.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="empty-page">
+        <div class="image-container">
+            <img src="@/assets/images/sea.svg" alt="Emptiness" />
+            <span>Nothing to see here</span>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+.empty-page {
+    display: grid;
+    grid-template-columns: 3fr 6fr 3fr;
+    height: 80vh;
+    align-items: center;
+    text-align: center;
+    .image-container {
+        grid-column: 2;
+        display: grid;
+        justify-content: center;
+        img {
+            width: 100%;
+            max-width: 500px;
+        }
+        span {
+            font-weight: 500;
+            color: #6d4c40;
+            margin-top: 2rem;
+        }
+    }
+}
+</style>
--- src/views/Torrent.vue
+++ src/views/Torrent.vue
@@ -1,16 +1,19 @@
 <template>
     <top-bar />
     <torrent-input />
+    <empty-page />
 </template>
 
 <script>
 import TopBar from '@/components/TopBar';
 import TorrentInput from '@/components/TorrentInput';
+import EmptyPage from '@/components/EmptyPage';
 
 export default {
     components: {
         TopBar,
-        TorrentInput
+        TorrentInput,
+        EmptyPage
     }
 };
 </script>
