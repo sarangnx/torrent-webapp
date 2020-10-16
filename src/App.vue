1ae7dee86d4738672f485739129e2035dbadfba1
Sarang <sarang98divakaran@gmail.com>
2020-10-16 14:27:47 +0530

    🌟 feat: added notification component to root of app

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,14 @@
 <template>
+    <notifications />
     <router-view />
 </template>
+
+<script>
+import Notifications from '@/components/Notifications/Notifications';
+
+export default {
+    components: {
+        Notifications
+    }
+};
+</script>
