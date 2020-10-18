28bbfc91e5800b0b2c8bace4315a8821538c5f33
Sarang <sarang98divakaran@gmail.com>
2020-10-18 12:30:31 +0530

    🌟 feat: added torrent list component

---
+++ src/components/TorrentList.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="torrent-list">
+        <torrent-item
+            v-for="torrent in torrents"
+            :key="torrent.infoHash"
+            :name="torrent.name"
+            :size="torrent.length"
+            :fileCount="torrent.files.length"
+        />
+    </div>
+</template>
+
+<script>
+import TorrentItem from './TorrentItem';
+
+export default {
+    components: {
+        TorrentItem
+    },
+    props: {
+        torrents: Array
+    }
+};
+</script>
+
+<style lang="scss">
+.torrent-list {
+    display: grid;
+    grid-template-columns: 1fr 10fr 1fr;
+    margin-top: 4rem;
+}
+</style>
