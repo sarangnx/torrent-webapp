a13b2a0b73006b1074bb31705a776e18f276cc1a
Sarang <sarang98divakaran@gmail.com>
2020-10-18 12:33:20 +0530

    🌟 feat: torrent list, usetorrent hook added in main component

--- src/views/Torrent.vue
+++ src/views/Torrent.vue
@@ -1,19 +1,27 @@
 <template>
     <top-bar />
-    <torrent-input />
-    <empty-page />
+    <torrent-input @upload="addTorrent" />
+    <empty-page v-if="!torrents || !(torrents && torrents.length)" />
+    <torrent-list :torrents="torrents" />
 </template>
 
 <script>
 import TopBar from '@/components/TopBar';
 import TorrentInput from '@/components/TorrentInput';
 import EmptyPage from '@/components/EmptyPage';
+import TorrentList from '@/components/TorrentList';
+import useTorrent from '@/hooks/useTorrent';
 
 export default {
     components: {
         TopBar,
         TorrentInput,
-        EmptyPage
+        EmptyPage,
+        TorrentList
+    },
+    setup() {
+        const { torrents, addTorrent } = useTorrent();
+        return { torrents, addTorrent };
     }
 };
 </script>
