97b3e2bf0ca36d9a189ee55cd5b84d241533022f
Sarang <sarang98divakaran@gmail.com>
2020-10-20 21:31:40 +0530

    ✨ refactor: changed how folder is opened - change route on click - watch route changes and change folder

--- src/components/FileExplorer.vue
+++ src/components/FileExplorer.vue
@@ -5,12 +5,13 @@
             :key="file.name"
             :is="file.type"
             :item="file"
-            @open-folder="openFolder"
+            @open-folder="changeRoute"
         ></component>
     </div>
 </template>
 
 <script>
+import { watch } from 'vue';
 import useFileExplorer from '@/hooks/useFileExplorer';
 import Folder from './Folder';
 import File from './File';
@@ -22,12 +23,21 @@ export default {
     },
     props: {
         files: Array,
-        root: String
+        root: String,
+        torrent: String,
+        paths: [String, Array]
     },
     setup(props) {
-        const { tree, openFolder, currentFolder } = useFileExplorer(props.files, props.root);
+        const { tree, openFolder, currentFolder, changeRoute } = useFileExplorer(props.files, props.root);
 
-        return { tree, openFolder, currentFolder };
+        // open folder when route changes
+        watch([props], () => {
+            if (props.paths && props.paths.length !== 0) {
+                openFolder(props.paths);
+            }
+        });
+
+        return { tree, openFolder, currentFolder, changeRoute };
     }
 };
 </script>
