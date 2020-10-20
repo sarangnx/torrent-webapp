ad8846e56c59eb2d2b0cc5acc8c60a141c21c07f
Sarang <sarang98divakaran@gmail.com>
2020-10-20 13:00:40 +0530

    🌟 feat: file explorer component to show list of files & folders

---
+++ src/components/FileExplorer.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="file-explorer">
+        <component
+            v-for="file in tree.children"
+            :key="file.name"
+            :is="file.type"
+            :file="file"
+            :folder="file.name"
+            :children="file.children"
+        ></component>
+    </div>
+</template>
+
+<script>
+import useFileExplorer from '@/hooks/useFileExplorer';
+import Folder from './Folder';
+import File from './File';
+
+export default {
+    components: {
+        Folder,
+        File
+    },
+    props: {
+        files: Array,
+        root: String
+    },
+    setup(props) {
+        const { tree, File, Folder } = useFileExplorer(props.files, props.root);
+
+        return { tree, File, Folder };
+    }
+};
+</script>
