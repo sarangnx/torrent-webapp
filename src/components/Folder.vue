af2f2914e5bee6d3e32f37dde737c0c993c357db
Sarang <sarang98divakaran@gmail.com>
2020-10-20 22:45:37 +0530

    🌟 feat: added folder icon

--- src/components/Folder.vue
+++ src/components/Folder.vue
@@ -1,5 +1,16 @@
 <template>
     <div class="folder" @click.stop="openFolder(item.path)">
+        <span class="folder-icon">
+            <svg width="1em" height="1em" viewBox="0 0 16 16">
+                <path
+                    d="M9.828 4a3 3 0 0 1-2.12-.879l-.83-.828A1 1 0 0 0 6.173 2H2.5a1 1 0 0 0-1 .981L1.546 4h-1L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3v1z"
+                />
+                <path
+                    fill-rule="evenodd"
+                    d="M13.81 4H2.19a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91h10.348a1 1 0 0 0 .995-.91l.637-7A1 1 0 0 0 13.81 4zM2.19 3A2 2 0 0 0 .198 5.181l.637 7A2 2 0 0 0 2.826 14h10.348a2 2 0 0 0 1.991-1.819l.637-7A2 2 0 0 0 13.81 3H2.19z"
+                />
+            </svg>
+        </span>
         <span class="folder-name">{{ item.name }}</span>
         <span class="folder-count">{{ item.count }} Files</span>
         <button class="folder-download button" @click.stop @dblclick.stop>
