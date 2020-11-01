43d12ed0ed8cfe1c5108602ab0d716459763680b
Sarang <sarang98divakaran@gmail.com>
2020-11-01 22:26:33 +0530

    🌟 feat: message event handler

--- src/plugins/socket.js
+++ src/plugins/socket.js
@@ -1,4 +1,5 @@
 import io from 'socket.io-client';
+import store from '@/store';
 
 class SocketIO {
     constructor() {
@@ -6,14 +7,33 @@ class SocketIO {
         this.socket = io(process.env.VUE_APP_API_URL, {
             transports: ['websocket', 'polling']
         });
+
+        this.init();
     }
 
+    // use this module as vue.js plugin
     install(app) {
         app.config.globalProperties.$io = this.socket;
     }
 
+    init() {
+        this.socket.on('reconnect', () => {
+            // rejoin the room on reconnect
+            if (this.roomId) {
+                this.join(this.roomId);
+            }
+        });
+
+        this.socket.on('message', data => {
+            store.dispatch('notifications/notify', {
+                message: data.message,
+                type: data.type
+            });
+        });
+    }
+
     /**
-     * Connected Room ID
+     * Connected Room ID.
      */
     roomId = null;
 
